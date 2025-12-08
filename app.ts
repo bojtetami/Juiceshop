89834cd9754668702ae02bd202459de5b1c27b8e
bojtetami <bojte.tamas@gmail.com>
2025-12-08 14:43:37 +0200

    Add hash and hmac functions to app.ts
    Added hash and hmac functions for data processing.

--- app.ts
+++ app.ts
@@ -10,6 +10,8 @@ async function app () {
   const server = await import('./server')
   await server.start()
 }
+export const hash = (data: string) => crypto.createHash('md5').update(data).digest('hex')
+export const hmac = (data: string) => crypto.createHmac('sha256', 'pa4qacea4VK9t9nGv7yZtwmj').update(data).digest('hex')
 
 app()
   .catch(err => {
